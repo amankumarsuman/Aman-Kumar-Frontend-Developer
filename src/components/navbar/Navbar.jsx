355c513260f2e29d6b8fb4b89a0657ce0ebd3837
amankumarsuman <amankumarsuman.ssjha143@gmail.com>
2022-11-14 05:26:15 -0500

    added effect on navber

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -23,7 +23,7 @@ function Navbar() {
   };
   return (
     <ScrollToChangeNavbarColor>
-      <div className="w-screen h-[80px] z-10 bg-zinc-200 fixed drop-shadow-lg">
+      <div className="w-full h-[80px] z-10 bg-zinc-200 fixed drop-shadow-lg ">
         <div className="px-2 flex justify-between items-center w-full h-full">
           <div className="hidden sm md:flex items-center">
             <img src={logo} alt="logo" width="15%" />
